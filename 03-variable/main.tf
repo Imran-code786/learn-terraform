0bc2c51e6b5f54cc553a86ac04e0863c95586cae
Imran <imrankhanmohammed130@gmail.com>
2026-05-03 17:03:52 -0500

    learning terraformhc

--- 03-variable/main.tf
+++ 03-variable/main.tf
@@ -2,40 +2,6 @@ variable "x" {
   default = "abc"
 }
 
-variable "y" {
-  default = 10
-}
-
-variable "z" {
-  default = false
-}
-
-variable "example1" {
-  default = [1,"a",false,2]
-
-}
-
-variable "example2" {
-  default = {
-    a = "f00"
-    b = 123
-    x = "bar"
-  }
-}
-
-output "x" {
+output "x"{
   value = var.x
-}
-
-output "x1" {
-  value = "x is ${var.x}"
-
-}
-
-output "example1" {
-  value = var.example1 [2]
-}
-
-output "example2" {
-  value = var.example2["a"]
 }
