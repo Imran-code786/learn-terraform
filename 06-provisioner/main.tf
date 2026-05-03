a8f17281743a04465430bbfe3a18a2f065bd5405
Imran <imrankhanmohammed130@gmail.com>
2026-05-03 17:58:40 -0500

    learning terraformhcsd

---
+++ 06-provisioner/main.tf
@@ -0,0 +1,19 @@
+
+resource "aws_instance" "test1" {
+  ami                          = "ami-0220d79f3f480ecf5"
+  instance_type                = "t3.small"
+  vpc_security_group_ids       = ["sg-04692f74cd3fa91ac"]
+
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      host     = self.public_ip
+      user     = "ec2-user"
+      password = "DevOps321"
+    }
+    inline = [
+      "echo Hello >/tmp/some"
+    ]
+  }
+}
