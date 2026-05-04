8c57926eb07393078f31bc1c2d952d24670ccd2a
Imran <imrankhanmohammed130@gmail.com>
2026-05-03 20:43:31 -0500

    learning terraformhcsd

---
+++ 07-null-resource-provisioner/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test" {
+  ami                  = "ami-0220d79f3f480ecf5"
+  instance_type        = "t3.small"
+  vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+}
+
+resource "null_resource" "test" {
+  triggers    = {
+    always  = timestamp()
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type   = "ssh"
+      host   = aws_instance.test.public_ip
+      user  = "ec2-user"
+      password = "DevOps321"
+    }
+    inline = [
+        " echo Hello >/tmp/some"
+
+    ]
+  }
+}
