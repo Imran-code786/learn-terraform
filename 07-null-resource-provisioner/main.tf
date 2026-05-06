0927ebf734a10af67738d2fa86bc623a9fd84145
Imran <imrankhanmohammed130@gmail.com>
2026-05-06 11:46:02 -0500

    learning terraformhcsddzsa

--- 07-null-resource-provisioner/main.tf
+++ 07-null-resource-provisioner/main.tf
@@ -1,25 +1,23 @@
-resource "aws_instance" "test" {
-  ami                  = "ami-0220d79f3f480ecf5"
-  instance_type        = "t3.small"
+resource "aws_instance" "Name" {
+  ami = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.small"
   vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
 }
 
 resource "null_resource" "test" {
-  triggers    = {
-    always  = timestamp()
-  }
+
 
   provisioner "remote-exec" {
     connection {
-      type   = "ssh"
-      host   = aws_instance.test.public_ip
-      user  = "ec2-user"
+      type     = "ssh"
+      host     = aws_instance.Name.public_ip
+      user     = "ec2-user"
       password = "DevOps321"
-    }
-    inline = [
-        " echo Hello >/tmp/some",
-        "echo Hllo >/tmp/some1"
 
+    }
+
+    inline  =  [
+       "echo Hello >/tmp/some"
     ]
   }
 }
