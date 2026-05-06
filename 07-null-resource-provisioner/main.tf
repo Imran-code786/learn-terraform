91b0670382d83efe605c0ca2489e4dad4f11aa0f
Imran <imrankhanmohammed130@gmail.com>
2026-05-06 11:50:35 -0500

    learning terraformhcsddzsa

--- 07-null-resource-provisioner/main.tf
+++ 07-null-resource-provisioner/main.tf
@@ -2,6 +2,10 @@ resource "aws_instance" "Name" {
   ami = "ami-0220d79f3f480ecf5"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-04692f74cd3fa91ac"]
+
+  tags = {
+    Name = "Alieotech"
+  }
 }
 
 resource "null_resource" "test" {
